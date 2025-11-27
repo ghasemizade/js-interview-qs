99d9cae92efc136314590d6a6a848afd8b571f8e
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-11-27 16:42:20 +0330

    task one (counter)

---
+++ tasks/counter/app.js
@@ -0,0 +1,18 @@
+
+
+function makeCounter(initVal = 0){
+    let current = initVal;
+    return function() {
+        const result = current;
+        current += 1;
+        return result;
+    }
+}
+
+const counter = makeCounter();
+counter();
+counter();
+counter();
+counter();
+
+console.log(counter())
