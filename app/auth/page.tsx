0884b6fe5b1b8780ea4edf2566e751d2be6e68a9
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2026-01-05 20:19:25 +0330

    styled: change bg color

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,7 +2,7 @@ import { AuthForm } from "@/components/auth-pages/auth-form"
 
 export default function AuthPage() {
     return (
-        <main className="min-h-screen flex items-center justify-center p-4">
+        <main className="min-h-screen flex items-center justify-center p-4 bg-linear-to-br from-slate-900 via-slate-800 to-slate-900">
             <div className="bg-white rounded-xl shadow-lg p-8 w-full max-w-md">
                 <AuthForm />
             </div>
